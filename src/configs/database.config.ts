314aaadf190ea052fd67a1fe4fe332f09db52868
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-05-30 12:23:17 +0300

    feat(config):remove database provider

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -7,6 +7,6 @@ export default registerAs(
 		port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
 		username: process.env.DB_USERNAME || 'postgres',
 		password: process.env.DB_PASSWORD || 'postgres',
-		database: process.env.DB_NAME || 'demo',
+		name: process.env.DB_NAME || 'demo',
 	}),
 );
