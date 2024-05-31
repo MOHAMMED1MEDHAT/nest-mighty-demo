09f03fb0f1fb97ec2a375bad48f38b261d4957db
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-05-31 10:56:07 +0300

    feat(task):update user to meet gql req

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities';
+import { UserRepository } from './repositories/user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private userRepository: UserRepository) {}
+
+	getUsers(query: unknown): Promise<User[]> {
+		return this.userRepository.find(query || {});
+	}
+
+	getUserById(userId: number): Promise<User> {
+		return this.userRepository.getUserById(userId);
+	}
+}
