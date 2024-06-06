a8b65dac111d9665c8ab1852435c9f387e793d59
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-06-06 12:03:04 +0300

    feat(history):update activity interceptor to meet new requirements

--- src/shared/interceptors/activity.interceptor.ts
+++ src/shared/interceptors/activity.interceptor.ts
@@ -5,6 +5,7 @@ import {
 	Logger,
 	NestInterceptor,
 } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { Observable, map } from 'rxjs';
 import { History } from 'src/history/entities/history.entity';
 import { HistoryRepository } from 'src/history/repositories/history.repository';
@@ -15,8 +16,8 @@ import { HistoryRepository } from 'src/history/repositories/history.repository';
 // 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 //     const request = context.switchToHttp().getRequest();
 // 		const history = new History();
-// 		history.creator = request.user.id;
-// 		history.details = `user ${request.user.id} named:${request.user.firstName} ${request.user.lastName} has accessed the task module`;
+// 		history.creator = request.locals.user.id;
+// 		history.details = `locals.user ${request.locals.user.id} named:${request.locals.user.firstName} ${request.locals.user.lastName} has accessed the task module`;
 // 		this.historyRepository.createHistory(history);
 // 		return next.handle();
 // 	}
@@ -29,13 +30,12 @@ export class ActivityInterceptor implements NestInterceptor {
 	private logger = new Logger(ActivityInterceptor.name);
 	constructor(private historyRepository: HistoryRepository) {}
 	intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-		this.logger.debug('ActivityInterceptor called');
-		const request = context.switchToHttp().getRequest();
+		const user = GqlExecutionContext.create(context).getContext().user;
 		return next.handle().pipe<Observable<unknown>>(
 			map((data) => {
 				const history = new History();
-				history.creator = request.user.id;
-				history.details = `user ${request.user.id} named:${request.user.firstName} ${request.user.lastName} has accessed the task module and was given the following data ${JSON.stringify(data)}`;
+				history.creator = user.id;
+				history.details = `locals.user ${user.id} named:${user.firstName} ${user.lastName} has accessed the task module and was given the following data ${JSON.stringify(data)}`;
 				this.historyRepository.createHistory(history);
 				return data;
 			}),
